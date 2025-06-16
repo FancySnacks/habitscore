868842eba4a425975149b12e88eedcebe856369e
FancySnacks <adrian.urbaniak1336@gmail.com>
2025-06-16 18:41:16 +0200

    Finished dynamic calendar creation

--- src/habitscore/__main__.py
+++ src/habitscore/__main__.py
@@ -6,31 +6,70 @@ from task import TaskPreset
 from timeunit import Day, Year, Month, Week
 
 
+def is_leap_year(year: int) -> bool:
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+    return False
+
+
+def int_to_weekday(n: int) -> str:
+    mapping = {1: 'Monday',
+               2: 'Tuesday',
+               3: 'Wednesday',
+               4: 'Thursday',
+               5: 'Friday',
+               6: 'Saturday',
+               7: 'Sunday',
+               }
+
+    return mapping[n]
+
+
+def int_to_month(n: int) -> str:
+    mapping = {1: 'January',
+               2: 'February',
+               3: 'March',
+               4: 'April',
+               5: 'May',
+               6: 'June',
+               7: 'July',
+               8: 'August',
+               9: 'September',
+               10: 'October',
+               11: 'November',
+               12: 'December',
+               }
+
+    return mapping[n]
+
+
+NOW = datetime.now()
+CURRENT_YEAR: int = NOW.year
+
+MONTHS = {'January': 31,
+          'February': 29 if is_leap_year(CURRENT_YEAR) else 28,
+          'March': 31,
+          'April': 30,
+          'May': 31,
+          'June': 30,
+          'July': 31,
+          'August': 31,
+          'September': 30,
+          'October': 31,
+          'November': 30,
+          'December': 31}
+
+
 class Calendar:
     def __init__(self):
+        self.today: Day
         self.years: list[Year] = []
-        self.current_year: Year
 
     def load_calendar(self):
-        # first load from save file
-        now = datetime.now()
-        current_year: int = now.year
-
-        mc = {'January': 31,
-              'February': 29 if self.is_leap_year(current_year) else 28,
-              'March': 31,
-              'April': 30,
-              'May': 31,
-              'June': 30,
-              'July': 31,
-              'August': 31,
-              'September': 30,
-              'October': 31,
-              'November': 30,
-              'December': 31}
-
-        year_day_count = sum(mc.values())
-        year_week_count = year_day_count // 7
+        # first load from save file, only then create dynamically
+        # if load from save file: update all the way to current day
 
         c_month = 1
         c_week = 1
@@ -41,14 +80,17 @@ class Calendar:
         weeks: list[Week] = []
         days: list[Day] = []
 
-        for item in mc.items():
+        # Loop through months
+        for item in MONTHS.items():
             month_daycount = item[1]
+            day_of_the_week = 1
 
+            # Create Day object for every day in a given month
             for day_i in range(1, month_daycount + 1):
                 day_date = date(date.today().year, c_month, day_i)
                 weekday = day_date.isoweekday()
 
-                day = Day(name=self.int_to_weekday(weekday),
+                day = Day(name=int_to_weekday(weekday),
                           weekly_index=day_of_the_week,
                           monthly_index=day_i,
                           yearly_index=c_day,
@@ -60,59 +102,31 @@ class Calendar:
                 c_day += 1
                 day_of_the_week += 1
 
+                # Create Week object
                 if weekday == 7:
                     week = Week(c_week, days[:])
                     weeks.append(week)
                     days.clear()
                     day_of_the_week = 1
+                    c_week += 1
 
+            # Create Month object
             month = Month(item[0], c_month, weeks[:])
             months.append(month)
             weeks.clear()
 
             c_month += 1
 
-        # create a year
-        # create all 12 months
-        # create weeks dynamically in segments of 1-7 days
-        # then assign weeks to months
+        # Create Year object
+        year = Year(CURRENT_YEAR, months)
+        self.years.append(year)
+
+        self.today = self.get_today()
+
         # load each day preset
 
-    def is_leap_year(self, year: int) -> bool:
-        if year % 4 == 0:
-            if year % 100 == 0:
-                if year % 400 == 0:
-                    return True
-        return False
-
-    def int_to_weekday(self, n: int) -> str:
-        mapping = {1: 'Monday',
-                   2: 'Tuesday',
-                   3: 'Wednesday',
-                   4: 'Thursday',
-                   5: 'Friday',
-                   6: 'Saturday',
-                   7: 'Sunday',
-                   }
-
-        return mapping[n]
-
-    def int_to_month(self, n: int) -> str:
-        mapping = {1: 'January',
-                   2: 'February',
-                   3: 'March',
-                   4: 'April',
-                   5: 'May',
-                   6: 'June',
-                   7: 'July',
-                   8: 'August',
-                   9: 'September',
-                   10: 'October',
-                   11: 'November',
-                   12: 'December',
-                   }
-
-        return mapping[n]
+    def get_today(self) -> Day:
+        return self.years[-1].months[NOW.month - 1].days[NOW.day - 1]
 
 
 cal = Calendar()
--- src/habitscore/timeunit.py
+++ src/habitscore/timeunit.py
@@ -19,13 +19,17 @@ class TimeUnit(ABC):
         print(f"{self.get_total_score()}/{self.get_potential_score()}")
 
 
-@dataclass
+@dataclass(order=True)
 class Day(TimeUnit):
+    _sort_index: int = field(init=False, repr=False)
     name: str
     weekly_index: int
     monthly_index: int
     yearly_index: int
     tasks: TaskPreset
+
+    def __post_init__(self):
+        self._sort_index = self.monthly_index
 
     def get_potential_score(self) -> int:
         return sum(task.score for task in self.tasks.tasks)
@@ -33,11 +37,18 @@ class Day(TimeUnit):
     def get_total_score(self) -> int:
         return sum(task.score for task in self.tasks.tasks if task.is_completed)
 
+    def __repr__(self) -> str:
+        return f"{self.name} {self.monthly_index}, weekly = {self.weekly_index}, yearly = {self.yearly_index}"
 
-@dataclass
+
+@dataclass(order=True)
 class Week(TimeUnit):
+    _sort_index: int = field(init=False, repr=False)
     index: int
     days: list[Day]
+
+    def __post_init__(self):
+        self._sort_index = self.index
 
     def get_potential_score(self) -> int:
         return sum(day.get_potential_score() for day in self.days)
@@ -46,11 +57,22 @@ class Week(TimeUnit):
         return sum(day.get_total_score() for day in self.days)
 
 
-@dataclass
+@dataclass(order=True)
 class Month(TimeUnit):
+    _sort_index: int = field(init=False, repr=False)
     name: str
     index: int
     weeks: list[Week]
+    days: list[Day] = field(init=False, default_factory=list)
+
+    def __post_init__(self):
+        self._sort_index = self.index
+
+        days = [week.days for week in self.weeks]
+        days_combined = []
+        [days_combined.extend(d) for d in days]
+        self.days = sorted(days_combined)
+
 
     def get_potential_score(self) -> int:
         return sum(week.get_potential_score() for week in self.weeks)
@@ -70,5 +92,5 @@ class Year(TimeUnit):
     def get_total_score(self) -> int:
         return sum(month.get_total_score() for month in self.months)
 
-    def today(self) -> str:
+    def s_today(self) -> str:
         return f"{len(self.months[-1].weeks[-1].days)} {self.months[-1]} {self.year}"
